154c923fbb4b04a2bc9a10d05b16147459b97081
“Brandon <Brandon.joseph.martinez3@gmail.com>
2022-05-19 12:09:28 -0500

    initial commit with code

---
+++ main.py
@@ -0,0 +1,20 @@
+import PyPDF2 as pdfr
+import pyttsx3 as pts
+
+
+# Split pdf by page then reads text on each page
+pdfReader = pdfr.PdfFileReader(open("evidence-based-resistance-training-rec_fisher.pdf", "rb"))
+
+# Converts text to speech
+speaker = pts.init()
+
+# Loop the process for each page, stops the speaker
+for page_num in range(pdfReader.numPages):
+    text = pdfReader.getPage(page_num).extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+speaker.stop()
+
+# Save the audio as an mp3 file
+speaker.save_to_file(text, "evidence-based-resistance-training-rec_fisher.mp3")
+speaker.runAndWait()
